96728fead199c172a78c627ec241e283ad9fef93
John Jackson <johnjackson@johns-mbp.lan>
2020-10-20 23:21:31 -0500

    First commit

---
+++ cmake-build-debug/CMakeFiles/rizkLab2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rizkLab2.dir/main.cpp.o"
+  "rizkLab2"
+  "rizkLab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rizkLab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
